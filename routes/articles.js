734a96865adc53aee23e323d00c00e5c2b241729
JSlovak <jasmine.slovak@gmail.com>
2017-04-19 18:46:00 -1000

    /article routes mapped.

--- routes/articles.js
+++ routes/articles.js
@@ -12,17 +12,36 @@ router.use((req, res, next) => {
 
 // '/articles' Route
 router.route('/')
+  .get(function(req, res) {
+    res.send("Successfully hit the GET end of /articles route!");
+  })
   .post(function(req, res) {
-    res.send("Successfully hit the end of /articles route!");
+    res.send("Successfully hit the POST end of /articles route!");
   });
 
-// '/products/:id' Route
+// '/articles/:titles' Route
 router.route('/:title')
-  .post(function(req, res) {
-    res.send("Successfully hit the end of POST /products/:id route!");
+  .get(function(req, res) {
+    res.send("Successfully hit the end of GET /articles/:titleroute!");
+  })
+  .put(function(req, res) {
+    res.send("Successfully hit the end of POST /articles/:title route!");
   })
   .delete(function(req, res) {
-    res.send("Successfully hit the end of DELETE /products/:id route!");
+    res.send("Successfully hit the end of DELETE /articles/:title route!");
   });
+
+// '/articles/:title/edit' Route
+router.route('/:title/edit')
+  .get(function(req, res) {
+    res.send("Successfully hit the end of GET /articles/:title/edit route!");
+  });
+
+// '/articles/new Route
+router.route('/new')
+  .get(function(req, res) {
+    res.send("Successfully hit the end of GET /articles/new route!");
+  });
+
 
 module.exports = router;
